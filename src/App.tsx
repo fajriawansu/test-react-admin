06953419e1249dba7356f90ac80181649ea4a67b
fajriawansu <fajriawan.sutansa@bluebirdgroup.com>
2024-05-14 17:13:32 +0700

    full test feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
-
-import { Admin, Resource, ListGuesser, EditGuesser, ShowGuesser } from 'react-admin';
-import { dataProvider } from './dataProvider';
+import {
+  Admin,
+  Resource,
+  ListGuesser,
+  EditGuesser,
+  ShowGuesser,
+} from "react-admin";
+import { dataProvider } from "./dataProvider";
+import { UserList } from "./components/UsersList";
+import { PostList } from "./components/PostsList";
+import { PostEdit } from "./components/PostEdit";
+import { PostCreate } from "./components/PostCreate";
 
 export const App = () => (
-    <Admin
-        dataProvider={dataProvider}
-	>
-        
-    </Admin>
+  <Admin dataProvider={dataProvider}>
+    {/* <Resource name="users" list={ListGuesser} /> */}
+    <Resource
+      name="users"
+      list={UserList}
+      recordRepresentation="name"
+      show={ShowGuesser}
+    />
+    <Resource
+      name="posts"
+      list={PostList}
+      edit={PostEdit}
+      create={PostCreate}
+    />
+  </Admin>
 );
-
-    
---
+++ src/components/PostsList.tsx
@@ -0,0 +1,19 @@
+import {
+  List,
+  Datagrid,
+  TextField,
+  ReferenceField,
+  EditButton,
+} from "react-admin";
+
+export const PostList = () => (
+  <List>
+    <Datagrid>
+      <TextField source="id" />
+      <ReferenceField source="userId" reference="users" link="show" />
+      <TextField source="title" />
+      <TextField source="body" />
+      <EditButton />
+    </Datagrid>
+  </List>
+);
